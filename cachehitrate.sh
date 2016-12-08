3fadbc5a7bde0e97e99670e5944e4ec808c2be63
Mike Schiessl <michael.schiessl@maxdome.de>
2016-12-08 09:04:50 +0100

    added "dirty" last 1000 req output
    needs to be put in a func

--- cachehitrate.sh
+++ cachehitrate.sh
@@ -12,9 +12,9 @@
 ###########
 
 # INIT
-t_miss=0
-t_hit=0
-t_expired=0
+t_miss=1
+t_hit=1
+t_expired=1
 
 
 # preflight checks
@@ -23,9 +23,9 @@ if [ -z "$1" ] ; then
 	exit 1
 fi
 
-# go parse 
-for line in `awk '{print $3}' $1 | sort | uniq -c | sort -r` ; do
-
+# go parse
+#for line in `cat $1 awk '{print $3}' $1 | sort | uniq -c | sort -r` ; do
+for line in `cat $1 | awk '{print $3}' | sort | uniq -c | sort -r` ; do
 	case $line in
 		HIT)
 			t_hit=$tmpvar
@@ -44,20 +44,60 @@ for line in `awk '{print $3}' $1 | sort | uniq -c | sort -r` ; do
 
 done
 
-# go calculate
-all_reqs=$(echo "($t_hit + $t_expired + $t_miss)" | bc -l)
 
+# go calculate
+all_reqs=$(echo "($t_hit + $t_expired + $t_miss)" | bc -l )
 hit_ratio=$(echo "$t_hit / $all_reqs * 100" | bc -l)
 miss_ratio=$(echo "$t_miss / $all_reqs * 100" | bc -l)
 expired_ratio=$(echo "$t_expired / $all_reqs * 100" | bc -l)
 
 
 
+# go output
+echo -e "\nOVERALL STATS"
+echo -e "HIT ratio:\t$hit_ratio \t\t($t_hit hits)"
+echo -e "MISS ratio:\t$miss_ratio \t\t($t_miss misses)"
+echo -e "EXPIRY ratio:\t$expired_ratio \t\t($t_expired expires)"
+
+
+
+t_miss=1
+t_hit=1
+t_expired=1
+
+
+for line in `tail -n 10000 $1 | awk '{print $3}' | sort | uniq -c | sort -r` ; do
+	case $line in
+		HIT)
+			t_hit=$tmpvar
+		;;
+		MISS)
+			t_miss=$tmpvar
+		;;
+		EXPIRED)
+			t_expired=$tmpvar
+		;;
+		*)
+			tmpvar=$line
+		;;
+	esac
+
+done
+
+# go calculate
+all_reqs=$(echo "($t_hit + $t_expired + $t_miss)" | bc -l)
+hit_ratio=$(echo "$t_hit / $all_reqs * 100" | bc -l)
+miss_ratio=$(echo "$t_miss / $all_reqs * 100" | bc -l)
+expired_ratio=$(echo "$t_expired / $all_reqs * 100" | bc -l)
+
+
 
 # go output
-echo -e "HIT Ratio: \t $hit_ratio \t\t($t_hit hits)"
-echo -e "MISS ratio: \t $miss_ratio \t\t($t_miss misses)"
-echo -e "EXPIRY ratio: \t $expired_ratio \t\t($t_expired expires)"
+echo -e "\nLAST 1000"
+echo -e "HIT ratio:\t$hit_ratio \t\t($t_hit hits)"
+echo -e "MISS ratio:\t$miss_ratio \t\t($t_miss misses)"
+echo -e "EXPIRY ratio:\t$expired_ratio \t\t($t_expired expires)\n"
+
 
 
 # go home
